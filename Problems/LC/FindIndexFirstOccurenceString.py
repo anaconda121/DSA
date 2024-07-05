9f99c41259514b2f98ae63429437bb8292596355
Tanish Tyagi <38384138+anaconda121@users.noreply.github.com>
2024-07-05 10:52:05 -0400

    Create FindIndexFirstOccurenceString.py

---
+++ Problems/LC/FindIndexFirstOccurenceString.py
@@ -0,0 +1,6 @@
+class FindIndexFirstOccurenceString:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(0, len(haystack) - len(needle) + 1, 1):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+        return -1

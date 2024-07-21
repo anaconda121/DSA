9599e7179ecdcff43636382322d3aee8647bb312
Tanish Tyagi <38384138+anaconda121@users.noreply.github.com>
2024-07-21 09:50:25 -0400

    Delete Problems/LC/out/production/LC directory

--- Problems/LC/out/production/LC/HeightChecker.py
+++
@@ -1,7 +0,0 @@
-class HeightChecker:
-    def heightChecker(self, heights: List[int]) -> int:
-        expected = sorted(heights)
-        ans = 0
-        for i in range(len(expected)): 
-          if (expected[i] != heights[i]): ans += 1
-        return ans

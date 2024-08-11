e2b5af1860362a085486a6c595ed7f0b62fdc2c0
Tanish Tyagi <38384138+anaconda121@users.noreply.github.com>
2024-08-11 14:27:50 -0400

    Create RangeSumSubarraySums.java

---
+++ Problems/LC/RangeSumSubarraySums.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        int mod = (int) (1e9 + 7);
+        List<Integer> subarraySums = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int currSum = 0;
+            for (int j = i; j < n; j++) {
+                currSum += nums[j];
+                subarraySums.add(currSum);
+            }
+        }
+
+        Collections.sort(subarraySums);
+
+        int sum = 0;
+        for (int i = left - 1; i < right; i++) {
+            sum = (sum + subarraySums.get(i)) % mod;
+        }
+        return sum;
+    }
+}
